55ef580efe91f8861b7e1a7442a1582d1fd73b24
Kushal <tokushal718@gmail.com>
2026-03-13 19:02:18 +0530

    Initial Jenkins lab setup

---
+++ backend/app.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <cstring>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+
+int main() {
+    char hostname[256];
+    gethostname(hostname, sizeof(hostname));
+    hostname[255] = '\0';
+
+    int server_fd = socket(AF_INET, SOCK_STREAM, 0);
+    int opt = 1;
+    setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
+
+    struct sockaddr_in address;
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_port = htons(8080);
+
+    bind(server_fd,(struct sockaddr *)&address,sizeof(address));
+    listen(server_fd,10);
+
+    std::cout<<"Server running on "<<hostname<<std::endl;
+
+    while(true){
+        int client_fd=accept(server_fd,NULL,NULL);
+
+        std::string response =
+        "HTTP/1.1 200 OK\r\n"
+        "Content-Type: text/plain\r\n\r\n"
+        "Served by backend: "+std::string(hostname)+"\n";
+
+        send(client_fd,response.c_str(),response.length(),0);
+        close(client_fd);
+    }
+}
